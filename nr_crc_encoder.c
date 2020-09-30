fe63ea79c62a368d0e123e0668aed2f00fac30c5
chawat <yash.rk@outlook.com>
2020-10-01 00:53:25 +0530

    CRC Enc: Long DIV Added

--- nr_crc_encoder.c
+++ nr_crc_encoder.c
@@ -1,5 +1,4 @@
-// NR_CRC_ENCODER - Perform CRC Encoding which is a required step right before
-//                  Polar Encoding
+// NR_CRC_ENCODER - Perform CRC Encoding
 
 #include <stdio.h>
 #include "polarCodes.h"
@@ -26,7 +25,7 @@ int * NR_CRC_ENCODER(int * dataBits, struct PC_CONFIG pcConfig) {
     }
 
     // Calculating Remainder to be appended to the message using Long Division
-    int * rem = poly_long_div(dataBits, crcPoly);
+    int * rem = poly_long_div(dataBits, crcPoly, pcConfig);
 
     // Appending Remainder to the Message Bits
     for (iter_bits = 0; iter_bits < (pcConfig.K - pcConfig.crcLen); iter_bits++) {
@@ -38,4 +37,58 @@ int * NR_CRC_ENCODER(int * dataBits, struct PC_CONFIG pcConfig) {
     }
 
     return crcEncOut;
+}
+
+// Performs Long Division between Two Polynomials
+int * poly_long_div(int * dataBits, int * crcPoly, struct PC_CONFIG pcConfig) {
+    int iter_bits, tmp, numBits = pcConfig.K - pcConfig.crcLen;
+    int * tmp_poly;
+    int * rem = (int *)malloc(sizeof(int) * numBits);
+
+    for (iter_bits = 0; iter_bits < (pcConfig.K - pcConfig.crcLen); iter_bits++) {
+        *(rem + iter_bits) = *(dataBits + iter_bits);
+    }
+
+    while((degree_poly(rem, numBits) - degree_poly(crcPoly, numBits)) >= 0) {
+        tmp = degree_poly(rem, numBits) - degree_poly(crcPoly, numBits);
+        tmp_poly = incr_degree_poly(crcPoly, pcConfig.crcLen + 1, tmp);
+
+        for (iter_bits = 0; iter_bits < numBits; iter_bits++) {
+            *(rem + iter_bits) = (*(rem + iter_bits) - *(tmp_poly + iter_bits)) % 2; 
+        }
+    }
+
+    return rem;
+}
+
+// Returns Degree of a Polynomial
+int degree_poly(int * poly, int numBits) {
+    // For iter var and return var
+    int iter_bits, deg;
+
+    // Iterating over the polynomial and finding the degree
+    for (iter_bits = 0; iter_bits < numBits; iter_bits++) {
+        if (*(poly + iter_bits)) {
+            deg = numBits - iter_bits;
+            break;
+        }
+    }
+
+    return deg;
+}
+
+// Increase the Degree of the Polynomial
+int * incr_degree_poly(int * poly, int numBits, int incr_deg) {
+    int iter_bits;
+    int * newPoly = (int * )malloc(sizeof(int) * (numBits + incr_deg));
+
+    for (iter_bits = 0; iter_bits < numBits; iter_bits++) {
+        *(newPoly + iter_bits) = *(poly + iter_bits);
+
+        if (iter_bits < incr_deg) {
+            *(newPoly + iter_bits + numBits) = 0;
+        }
+    }
+
+    return newPoly;
 }
--- polarCodes.h
+++ polarCodes.h
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+// Polar Codes
+
 struct PC_CONFIG {
     int E;
     int K;
@@ -19,21 +21,27 @@ int * NR_CRC_ENCODER(int * bits, struct PC_CONFIG pcConfig);
 int * NR_CRC_DECODER(int * bits, struct PC_CONFIG pcConfig);
 
 int * NR_PC_RATE_MATCH(int * bits, struct PC_CONFIG pcConfig);
-int * NR_PC_RATE_DEMATCH(int * bits, struct PC_CONFIG pcConfig);
-
-float * BPSK_MOD(int * bits);
-float * BPSK_DEMOD(float * syms, int BITS_LLR);
-
-float * AWGN(float * syms, float SNRdB);
-float * EPA1(float * syms, int numRxAnt, float dopplerFreq, float initTime);
+int * NR_PC_RATE_REMOVE(int * bits, struct PC_CONFIG pcConfig);
 
 int * SC_DECODER(float * bits, struct PC_CONFIG pcConfig);
 int * SCL_DECODER(float * bits, struct PC_CONFIG pcConfig);
 int * BP_DECODER(float * bits, struct PC_CONFIG pcConfig);
 
-int * DATA_GEN(int numBits);
+// BPSK Modulation
 
+float * BPSK_MOD(int * bits);
+float * BPSK_DEMOD(float * syms, int BITS_LLR);
+
+// Fading and AWGN Channels
+
+float * AWGN(float * syms, float SNRdB);
+float * EPA1(float * syms, int numRxAnt, float dopplerFreq, float initTime);
+
+// Utilities and Misc.
+
+int * DATA_GEN(int numBits);
 void PRINT_ARRAY_INT(int * dataBits, int numBits);
 void PRINT_ARRAY_FLOAT(float * mat);
-
+int * poly_long_div(int * dataBits, int * crcPoly, struct PC_CONFIG pcConfig);
+int degree_poly(int * poly, int numBits);
 
