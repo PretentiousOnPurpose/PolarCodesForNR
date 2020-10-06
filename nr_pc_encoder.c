e8f0b14f47511a79c20d34f9aa028185a3f448c3
chawat <yash.rk@outlook.com>
2020-10-06 16:58:59 +0530

    PC ENC UPDATE

--- nr_pc_encoder.c
+++ nr_pc_encoder.c
@@ -4,14 +4,16 @@
 #include "polarCodes.h"
 
 int * NR_PC_ENCODER(int * dataBits, struct PC_CONFIG * pcConfig) {
-    int iter_step, iter_bits;
+    int iter_step, iter_bits, iter_group;
     int * encData = (int *)calloc(1 << pcConfig->nMax, sizeof(int));
 
     encData = PC_SET_DATABITS(encData);
 
     for (iter_step = 1; iter_step < pcConfig->nMax; iter_step++) {
-        for (iter_bits = 0; iter_bits < (1 << (pcConfig->nMax - 1)); iter_bits += (1 << iter_step)) {
-            // encData = POLAR_TRANSFORM(encData, iter_bits, );
+        for (iter_group = 0; iter_group < (1 << (pcConfig->nMax - iter_step)); iter_group++) {
+            for (iter_bits = 0; iter_bits < (1 << (iter_step - 1)); iter_bits++) {
+                encData = POLAR_TRANSFORM(encData, iter_bits + iter_group * (1 << iter_step), iter_bits + iter_group * (1 << iter_step) + (1 << (iter_step - 1)));
+            }
         }
     }
 
